603a8c5162017e762b98003d4ec1d4e08877c118
JDgoldvox <voxcastJDgoldvox@gmail.com>
2025-03-12 22:34:34 +0000

    Q2 done

--- NumericalMethods.py
+++ NumericalMethods.py
@@ -1,32 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as npy
 
-stepSize = 40.50;
-total = 100.0;
-
-def func(x,y):
-    return 5.0 * x ** 4
-
-def ans(x,y):
-    return x**5
-
 #euler -----------------------------------------------------
 #Euler's method 
-#dy = m * dx
-#y(n+1) = y(n) + m * dx
 def euler(f, x0, y0, xe, dx):
     stepCount = num_steps(x0, xe, dx)
-    xx = npy.linspace(x0, xe, stepCount) #Add + dx to include last element
-    yy = npy.zeros(len(xx))
-    yy[0] = y0
-    for i in range(1, len(xx)):
-        yy[i] = yy[i-1] + dx * f(xx[i-1], yy[i-1])
-    return xx, yy
+    x_values = npy.linspace(x0, xe, stepCount) 
+    y_values = npy.zeros(len(x_values), len(y0))
+    y_values[0] = y0
+    
+    for i in range(1, len(x_values)):
+        y_values[i] = y_values[i-1] + dx * f(x_values[i-1], y_values[i-1])
+    return x_values, y_values
     
 #rk2 -----------------------------------------------------
-# k1 = dx * f(x,y)
-# k2 = dx * f(x(n) + dx/2, y(n) + k1/2)
-# y(n) + 1 = y(n) + k2
 def rk2(f, x0, y0, xe, dx):
     stepCount = num_steps(x0, xe, dx)
     xx = npy.linspace(x0, xe, stepCount)
@@ -39,58 +26,30 @@ def rk2(f, x0, y0, xe, dx):
     return xx, yy
     
 #rk4 -----------------------------------------------------
-# k1 = dx * f(x(n), y(n))
-# k2 = dx * f(x(n) + dx/2, y(n) + k1/2)
-# k3 = dx * f(x(n) + dx/2, y(n) + k2/2)
-# k4 = dx * f(x(n) + dx, y(n) + k3)
-# y(n+1) = y(n) + (k1 + 2k2, + 2k3 + k4 / 6)
-#xe = last time, dx = interval
-#x0 = time
+#xe = last time
+#x0 = time end
+#dx = interval
+#y0 = value (anguler velocity)
+#f = function
 def rk4(f, x0, y0, xe, dx):
     stepCount = num_steps(x0, xe, dx)
-    xx = npy.linspace(x0, xe, stepCount)
-    yy = npy.zeros(len(xx))
-    yy[0] = y0
-    for i in range(1, len(xx)):
-        k1 = dx * f(xx[i-1], yy[i-1]) 
-        k2 = dx * f(xx[i-1] + dx/2, yy[i-1] + k1/2)
-        k3 = dx * f(xx[i-1] + dx/2, yy[i-1] + k2/2)
-        k4 = dx * f(xx[i-1] + dx, yy[i-1] + k3)
-        yy[i] = yy[i -1] + ((k1 + 2 * k2 + 2 * k3 + k4) / 6)
-    return xx, yy
+    x_values = npy.linspace(x0, xe, stepCount) #create interval array
+    y_values = npy.zeros((len(x_values), len(y0))) #2d array
+    y_values[0] = y0 
+    
+    for i in range(1, len(x_values)):
+        k1 = dx * f(x_values[i-1], y_values[i-1]) 
+        k2 = dx * f(x_values[i-1] + dx/2, y_values[i-1] + k1/2)
+        k3 = dx * f(x_values[i-1] + dx/2, y_values[i-1] + k2/2)
+        k4 = dx * f(x_values[i-1] + dx, y_values[i-1] + k3)
+        y_values[i] = y_values[i -1] + ((k1 + 2 * k2 + 2 * k3 + k4) / 6)
+    return x_values, y_values
     
 def num_steps(x0, xe, dx):
     return int((xe - x0) / dx) + 1
-    
-euler_result_x, euler_result_y = euler(func, 0, 0, total, stepSize)
-rk2_result_x, rk2_result_y = rk2(func, 0, 0, total, stepSize)
-rk4_result_x, rk4_result_y = rk4(func, 0, 0, total, stepSize)
-
-stepCount =  num_steps(0, total, stepSize)
-xa = npy.linspace(0, total, stepCount)
-ya = xa**5.0
     
 #Plot -----------------------------------------------------
 #plt.plot(xa, ya, label = "real") #real answer
 #plt.plot(euler_result_x, euler_result_y, label = "euler")
 #plt.plot(rk2_result_x, rk2_result_y, label = "rk2")
 #plt.plot(rk4_result_x, rk4_result_y, label = "rk4")
-
-#print(".", ya[-1])
-#print(".", 20**5)
-#print(".", xa[-1])
-#print("Euler error: ", abs(ya[-1] - euler_result_y[-1]))
-#print("rk2 error: ", abs(ya[-1] - rk2_result_y[-1]))
-#print("rk4 error: ", abs(ya[-1] - rk4_result_y[-1]))
-
-#plt.legend()
-#plt.title(label="Euler vs rk2 vs rk4 results")
-#plt.show()
-
-#plt.plot(ya, abs(ya-euler_result_y), label = "euler")
-#plt.plot(ya, abs(ya-rk2_result_y), label = "rk2")
-#plt.plot(ya, abs(ya-rk4_result_y), label = "rk4")
-
-#plt.legend()
-#plt.title(label="Euler vs rk2 vs rk4 error difference")
-#plt.show()
--- main.py
+++ main.py
@@ -1,52 +1,66 @@
 import NumericalMethods
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as npy
 
 mass = 5
 radius = 2
 height = 6
-wx0, wy0, wz0 = 1, 2, 3
+w0 = 1, 2, 3
 timeStart, timeEnd = 0, 20
-steps = 2000;
+steps = 0.01;
 
-def main():
-    Ix, Iy, Iz = solve_intertia_tensor(mass, radius, height)
-    print(Ix)
-    print(Iy)
-    print(Iz)
-    print("---------------------------")
-    Wx, Wy, Wz = solve_euler_equation(Ix, Iy, Iz, wx0, wy0, wz0)
-    print(Wx)
-    print(Wy)
-    print(Wz)
-    
-    #f = Wx
-    #x0 = timeStart
-    #y0 = wx0
-    #xe = timeEnd
-    #dx = steps
-    xx, yy = NumericalMethods.rk4(Wx, timeStart, wx0, timeEnd, steps);
-    
-    
-    
-    xx, yy = NumericalMethods.rk4(Wx, timeStart, wx0, timeEnd, steps);
-    xx, yy = NumericalMethods.rk4(Wx, timeStart, wx0, timeEnd, steps);
-    
+velocity0 = 0, 0, 200
+acceleration = 0, 0, -9.8
 
 def solve_intertia_tensor(mass, radius, height):
-    Ix = (3/20) * mass * ((radius**2) + (1/4 * height**2))
-    Iy = (3/20) * mass * ((radius**2) + (1/4 * height**2))
-    Iz = (3/20) * mass * (2*radius**2)
-    return Ix, Iy, Iz
+    I1 = (3/20) * mass * ((radius**2) + (1/4 * height**2))
+    I2 = (3/20) * mass * ((radius**2) + (1/4 * height**2))
+    I3 = (3/20) * mass * (2*radius**2)
     
+    tensor = I1, I2, I3
+    return tensor
 
-def solve_euler_equation(I1, I2, I3,  wx, wy, wz):
+tensor = solve_intertia_tensor(mass, radius, height)
+
+#returns array of euler euqation results
+def solve_euler_equation(t,  w0):
+    I1, I2, I3 = tensor
+    wx0, wy0, wz0 = w0
+    
     gamma1 = (I3 - I2) / I1
     gamma2 = (I1 - I3) / I2
     gamma3 = (I2 - I1) / I3
     
-    Wx = -gamma1 * wy * wz
-    Wy = -gamma2 * wx * wz
-    Wz = -gamma3 * wx * wy
-    return Wx, Wy, Wz
+    Wx = -gamma1 * wy0 * wz0
+    Wy = -gamma2 * wx0 * wz0
+    Wz = -gamma3 * wx0 * wy0
+    
+    return npy.array([Wx, Wy, Wz])
 
+def main():
+    #f = solve_euler_equation
+    #x0 = timeStart
+    #y0 = w0
+    #xe = timeEnd
+    #dx = steps
+    xx, yy = NumericalMethods.rk4(solve_euler_equation, timeStart, w0, timeEnd, steps);
+    
+    #Grab values from each axis
+    wx_values = yy[:, 0]  #wx
+    wy_values = yy[:, 1]  #wy
+    wz_values = yy[:, 2]  #wz
+    
+    #2D Plot 
+    plt.figure()
+    plt.plot(xx, wx_values, label="wx")
+    plt.plot(xx, wy_values, label="wy")
+    plt.plot(xx, wz_values, label="wz")
+    plt.xlabel("Time")
+    plt.ylabel("Angular Velocity")
+    plt.legend()
+    plt.title("Task 2 - Anguler velocity over t [0,20]")
+    plt.show()
+    
 if __name__ == "__main__":
     main()
